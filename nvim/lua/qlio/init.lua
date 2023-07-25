bd52d36fa3e6dd667989398b34fba420cef36b6e
Bulgantamir Gankhuyag <bulgaadev@gmail.com>
2023-07-25 08:25:26 +0800

    terraform folding

--- nvim/lua/qlio/init.lua
+++ nvim/lua/qlio/init.lua
@@ -5,3 +5,23 @@ require("qlio.lsp")
 
 vim.g.user_emmet_leader_key = ','
 vim.g.user_emmet_instal_global = 0
+
+function TerraformFoldText()
+    local words = {}
+    for substring in vim.fn.getline(vim.v.foldstart):gmatch("[a-z_\"]+") do
+        table.insert(words, substring)
+    end
+    return words[1] .. ' ' .. words[2] .. ' ' .. words[3]
+end
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+    pattern = {"*.tf"},
+    callback = function()
+        local filetype = vim.bo.filetype
+        if filetype == "terraform" then
+            vim.wo.foldmethod = "expr"
+            vim.wo.foldtext = "v:lua.TerraformFoldText()"
+            vim.wo.foldexpr = "getline(v:lnum+1)=~'^\\l'?'<1':1"
+        end
+    end,
+})
--- nvim/lua/qlio/set.lua
+++ nvim/lua/qlio/set.lua
@@ -11,8 +11,9 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.lazyredraw = true   -- Do not redraw screen in the middle of a macro. Makes them complete faster.
-vim.opt.ignorecase = true   -- Searches are case insensitive
-vim.opt.smartcase = true    -- Searches with a capital characters are case sensitive.
-vim.opt.undofile = true     -- Persistent undo
-vim.opt.swapfile = false    -- Fuck swapfiles
+vim.opt.lazyredraw = true                 -- Do not redraw screen in the middle of a macro. Makes them complete faster.
+vim.opt.ignorecase = true                 -- Searches are case insensitive
+vim.opt.smartcase = true                  -- Searches with a capital characters are case sensitive.
+vim.opt.undofile = true                   -- Persistent undo
+vim.opt.swapfile = false                  -- Fuck swapfiles
+vim.opt.fillchars:append({ fold = " " })  -- Make foldtext more clean
