ed7c2e8116c88ab29df0866cea16a096e3ceda48
Bulgantamir Gankhuyag <bulgaadev@gmail.com>
2023-07-25 10:44:37 +0800

    custom typescript fold text

--- nvim/lua/qlio/init.lua
+++ nvim/lua/qlio/init.lua
@@ -6,22 +6,55 @@ require("qlio.lsp")
 vim.g.user_emmet_leader_key = ','
 vim.g.user_emmet_instal_global = 0
 
-function TerraformFoldText()
-    local words = {}
-    for substring in vim.fn.getline(vim.v.foldstart):gmatch("[a-z_\"]+") do
-        table.insert(words, substring)
-    end
-    return words[1] .. ' ' .. words[2] .. ' ' .. words[3]
-end
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    desc = "Remove trailing spaces",
+    command = [[%s/\s\+$//e]],
+})
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    pattern = {"*.tf"},
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.tf",
+    desc = "Custom terraform folding",
     callback = function()
-        local filetype = vim.bo.filetype
-        if filetype == "terraform" then
-            vim.wo.foldmethod = "expr"
-            vim.wo.foldtext = "v:lua.TerraformFoldText()"
-            vim.wo.foldexpr = "getline(v:lnum+1)=~'^\\l'?'<1':1"
-        end
+        vim.api.nvim_set_option_value("foldmethod", "expr", { scope = "local" })
+        vim.api.nvim_set_option_value("foldtext", "v:lua.TerraformFoldText()", { scope = "local" })
+        vim.api.nvim_set_option_value("foldexpr", "getline(v:lnum+1)=~'^\\l'?'<1':1", { scope = "local" })
     end,
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.ts",
+    desc = "Custom typescript folding",
+    callback = function()
+        vim.api.nvim_set_option_value("foldmethod", "expr", { scope = "local" })
+        vim.api.nvim_set_option_value("foldtext", "v:lua.TypeScriptFoldText()", { scope = "local" })
+        vim.api.nvim_set_option_value("foldexpr", "nvim_treesitter#foldexpr()", { scope = "local" })
+    end,
+})
+
+function TerraformFoldText()
+    local words = {}
+    for substring in vim.fn.getline(vim.v.foldstart):gmatch("[a-z_\"-]+") do
+        table.insert(words, substring)
+    end
+    return table.concat(words, ' ')
+end
+
+function TypeScriptFoldText()
+    local line = vim.fn.getline(vim.v.foldstart)
+    print(line:sub(-1))
+
+    if line:sub(-9) == "= () => {" or line:sub(-9) == "= () => (" then
+        return line:sub(0, -10)
+    end
+    if line:sub(-10) == "= () => ({" then
+        return line:sub(0, -11)
+    end
+    if line:sub(-3) == "= {" or line:sub(-3) == "= (" then
+        return line:sub(0, -4)
+    end
+    if line:sub(-1) == "(" or line:sub(-1) == "{" then
+        return line:sub(0, -2)
+    end
+    return line
+end
